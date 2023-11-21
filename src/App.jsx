1af9a61a1101e60b48dd3c8e5eddeeffc4da4469
joshrashtian <joshrashtian1@gmail.com>
2023-11-20 22:26:31 -0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 style={{ fontWeight: 200 }}>Joshua Rashtian.</h1>
+      <div className="card">
+        <p>
+          Future Programmer, Designer, Creative.
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Stay Tuned!
+      </p>
+    </>
+  )
+}
+
+
+export default App
