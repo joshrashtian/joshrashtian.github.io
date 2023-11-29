02ee6d59bf08e12747c71a785672a6f3debdf2f6
joshrashtian <joshrashtian1@gmail.com>
2023-11-29 13:28:07 -0800

    About and Journey Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 import Navigation from "./components/Navigation";
 import { Home } from "./components/Home";
 import { Projects } from "./components/Projects";
+import Journey from "./components/Journey";
+import About from "./components/About";
 
 function App () {
   return (
@@ -12,8 +14,8 @@ function App () {
     <div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<Home />} />
-          <Route path="/story" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/story" element={<Journey />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
       </div>
---
+++ src/components/Journey.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Journey = () => {
+  return (
+    <div>
+        <h1>Journey</h1>
+    </div>
+  )
+}
+
+export default Journey
